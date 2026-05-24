1fdfa64658dbe0a498db62fbd14c628b312ca627
Aaron <aaronblascog@gmail.com>
2026-05-25 01:57:04 +0200

    Dani puto

---
+++ Motos.java
@@ -0,0 +1,21 @@
+public class Motos extends Embarcacion{
+   private int cv;
+    private double litros;
+    public Motos(int id, String nombre, int precioBase, int cv, double litros) throws CaballosException{
+        super(id, nombre, precioBase);
+        if(cv<IDatos.MIN_CABALLOS || cv>IDatos.MAX_CABALLOS){
+            throw new CaballosException("Los caballos deben estar entre "+IDatos.MIN_CABALLOS+" y "+IDatos.MAX_CABALLOS);
+        }
+        this.cv = cv;
+        this.litros = litros;
+    }
+    public int getCv(){
+        return cv;
+    }
+    public double alquiler(){
+        return (getPrecioBase()+cv) *IDatos.ALQUILER_MOTOS;
+    }
+    public String toString(){
+        return super.toString()+" Motos [cv=" + cv + ", litros=" + litros + "]";
+    }
+}
