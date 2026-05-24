bec6e5068ca44544c21c51cc64b292caf5199037
targuslp-gif <targuslp@gmail.com>
2026-05-25 01:39:31 +0200

    GAUYAAAA

--- Embarcacion.java
+++ Embarcacion.java
@@ -1,3 +1,14 @@
-public class Enbarcacion {
-
+abstract class Embarcacion {
+protected int id;
+protected String nombre;
+protected int precioBase;
+public Embarcacion(int id, String nombre, int precioBase) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precioBase = precioBase;
+    }
+    public int getPrecioBase() {
+        return precioBase;
+    }
+    abstract double alquiler();
 }
